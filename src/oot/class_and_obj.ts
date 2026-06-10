3f387ca0d89e90eff78c6de500ebb6696ea86a14
MohammadRijuan <mohammadrijuan2529@gmail.com>
2026-06-11 00:10:42 +0600

    constructor

--- src/oot/class_and_obj.ts
+++ src/oot/class_and_obj.ts
@@ -5,9 +5,29 @@ class Animal {
     species:string;
     sound:string;
 
-    constructor (name:string, species:string, sound:string){
+    constructor ( name:string, species:string, sound:string){
         this.name = name;
         this.species = species;
         this.sound = sound;
     }
+
+    makeSound(){
+        return `${this.name} says ${this.sound}`;
+    }
 }
+
+
+const fahad = new Animal("fahad","kutto"," gew gew");
+
+console.log(fahad.makeSound());
+
+
+// more professional
+
+class Person {
+    constructor(public name: string, private age: number, private salary: number) {}
+}
+
+const person1 = new Person("fahad", 30, 50000);
+
+console.log(person1)
