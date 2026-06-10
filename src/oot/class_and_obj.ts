2fe1b851a082cfbdcf810187c526c5e3c369984e
MohammadRijuan <mohammadrijuan2529@gmail.com>
2026-06-10 23:00:55 +0600

    done

---
+++ src/oot/class_and_obj.ts
@@ -0,0 +1,13 @@
+// object orient typescript - oot/oop
+
+class Animal {
+    name:string;
+    species:string;
+    sound:string;
+
+    constructor (name:string, species:string, sound:string){
+        this.name = name;
+        this.species = species;
+        this.sound = sound;
+    }
+}
